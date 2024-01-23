56863801e0e9e75b1eb949fe7162ee8b170d7997
Borhan Uddin <68281712+wapborhan@users.noreply.github.com>
2024-01-23 21:11:06 +0600

    hbs remove

--- src/app.js
+++ src/app.js
@@ -5,7 +5,6 @@ const applyMiddleware = require("./middlewares");
 const path = require("path");
 const globalErrorHandler = require("./utils/globalErrorHandler");
 app.set("views", path.join(__dirname, "views"));
-// app.set("view engine", "hbs");
 app.set("view engine", "ejs");
 
 const leadRoutes = require("./routes/lead");
@@ -16,20 +15,13 @@ const paymentRoutes = require("./routes/payment");
 
 applyMiddleware(app);
 
-// Homepage
-// app.get("/", (req, res) => {
-//   res.render("Home", {
-//     message: "Rubel Auto Server Running...",
-//   });
-// });
-
 app.get("/", function (req, res) {
   res.render("pages/index");
 });
-// about page
 app.get("/about", function (req, res) {
   res.render("pages/about");
 });
+
 // All Routes
 app.use(leadRoutes);
 app.use(customerRoutes);
