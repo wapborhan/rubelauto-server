4fe7782f3afc603e36b4f14b8570d8f0e250f577
Borhan Uddin <68281712+wapborhan@users.noreply.github.com>
2024-02-04 19:53:13 +0600

    User Wise Customer Show

--- src/api/customer/controllers/allCustomer.js
+++ src/api/customer/controllers/allCustomer.js
@@ -3,7 +3,15 @@ const viewCustomer = require("../../../lib/customer/viewCustomer");
 
 const allCustomer = async (req, res) => {
   const cusData = req.params.status;
-  const cursor = { cardStatus: cusData };
+  const { showroom } = req.query;
+  let cursor;
+
+  if (showroom === "Head Office") {
+    cursor = { cardStatus: cusData };
+  } else {
+    cursor = { cardStatus: cusData, showRoom: showroom };
+  }
+
   const result = await viewCustomer(cursor);
 
   res.send(result);
