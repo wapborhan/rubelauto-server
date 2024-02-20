7b944f8e78d3321342bfa1eeedf4018c0276aa07
Borhan Uddin <68281712+wapborhan@users.noreply.github.com>
2024-02-20 20:26:58 +0600

    Limited Ip Access Site

--- src/app.js
+++ src/app.js
@@ -14,7 +14,22 @@ const stockRoutes = require("./routes/stock");
 const paymentRoutes = require("./routes/payment");
 const userRoutes = require("./routes/user");
 
+const allowedIPs = ["192.168.1.100", "192.168.1.101", "103.138.226.75", "::1"];
+
 applyMiddleware(app);
+
+// Middleware to check if the client's IP is allowed
+const IPFilterMiddleware = (req, res, next) => {
+  const clientIP = req.headers["x-forwarded-for"] || req.socket.remoteAddress; // Get client's IP address from the request
+  if (allowedIPs.includes(clientIP)) {
+    next(); // Allow the request to proceed
+  } else {
+    res.status(403).send("Access Forbidden"); // Return a 403 Forbidden status
+  }
+};
+
+// Apply IP filtering middleware to all routes
+app.use(IPFilterMiddleware);
 
 app.get("/", function (req, res) {
   res.render("pages/index");
